8cf3b5f93a1a227db51700973908407a03b6ff97
Adnan Wazwaz <adnanwazwaz1420@gmail.com>
2023-10-04 19:58:00 -0500

    Create word search, fetch word info

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-document.addEventListener("DOMContentLoaded", () => console.log("We are connected!"));
+const API_PREFIX = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
+
+function getWord(word) {
+    return fetch(`${API_PREFIX}${word}`)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            console.log(data.length);
+        });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("We are connected!");
+    document.getElementById('word-search-form').addEventListener('submit', (e) => {
+        e.preventDefault();
+        const word = e.target.word.value;
+        console.log(word);
+        console.log(getWord(word));
+    });
+});
