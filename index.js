caa8f910ed6732d6c8a06af3ded75a640ac23f43
Adnan Wazwaz <adnanwazwaz1420@gmail.com>
2023-10-04 21:54:24 -0500

    Display all definitions and examples as a table, with parts of speech

--- index.js
+++ index.js
@@ -6,16 +6,34 @@ const MEANINGS_INDEX = 1;
 // ADD HERE
 const DEFINITION_INDEX = 1;
 
+// Helper function for correct grammar when displaying data.
+const correctArticleForSpeechPart = function(partOfSpeech) {
+    switch (partOfSpeech.toLowerCase()) {
+        case 'adjective':
+        case 'article':
+        case 'adverb':
+        case 'interjection':
+            return 'an';
+        default:
+            return 'a';
+    }
+};
+
+const capitalize = function(word) {
+    return word[0].toUpperCase() + word.slice(1);
+};
+
 function getWord(word) {
     return fetch(`${API_PREFIX}${word}`)
         .then((response) => response.json())
         .then((data) => {
             console.log(data);
-            displayDefinitions(data);
+            //displayDefinitionsAsList(data);
+            displayDefinitionsAsTable(data);
         });
 }
 
-function displayDefinitions(data) {
+function displayDefinitionsAsList(data) {
     const definitionsList = document.getElementById('definitions-list');
     definitionsList.innerHTML = "";
     for (let i = 0; i < data.length; i++) {
@@ -26,6 +44,47 @@ function displayDefinitions(data) {
                 definitionsList.appendChild(li);
             }
         }
+    }
+}
+
+function displayDefinitionsAsTable(data) {
+    const definitionDisplay = document.getElementById('definition-display');
+    definitionDisplay.innerHTML = "";
+    for (let i = 0; i < data.length; i++) {
+        for (let j = 0; j < data[i]["meanings"].length; j++) {
+
+            const partOfSpeech = data[i]["meanings"][j]["partOfSpeech"];
+
+            // Create category
+            const category = document.createElement('h4');
+            category.textContent = `Category ${i + 1}.${j + 1} - As ${correctArticleForSpeechPart(partOfSpeech)} ${capitalize(partOfSpeech)}`;
+            const table = document.createElement('table');
+            table.innerHTML = `
+            <tr>
+                <th>#</th>
+                <th>Definition</th>
+                <th>Example(s)</th>
+            </tr>
+            `;
+
+            // Create table data
+            for (let k = 0; k < data[i]["meanings"][j]["definitions"].length; k++) {
+                const defEntry = document.createElement('tr');
+                // Retrieve example.
+                let example = data[i]["meanings"][j]["definitions"][k]["example"];
+                example = (example === undefined) ? "" : example;
+                defEntry.innerHTML = 
+                    `
+                    <td>${i+1}.${j+1}.${k+1}</td>
+                    <td>${data[i]["meanings"][j]["definitions"][k]["definition"]}</td>
+                    <td>${example}</td>
+                    `;
+                table.appendChild(defEntry);
+            }
+        definitionDisplay.appendChild(category);
+        definitionDisplay.appendChild(table);
+        }
+        
     }
 }
 
@@ -52,7 +111,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // Click Event for word
     document.getElementById('display-word').addEventListener('click', (e) => {
         try {
-            
+
         } catch (error) {
             console.error(error.message);
         }
