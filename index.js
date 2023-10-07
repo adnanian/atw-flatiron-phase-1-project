32d17bfe892ab195e7a82f8b6564058293e79d27
Adnan Wazwaz <adnanwazwaz1420@gmail.com>
2023-10-07 12:45:25 -0500

    Convert nested for loop for data retrieval to nested forEach

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 const API_PREFIX = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
 
-// ADD HERE
-const MEANINGS_INDEX = 1;
-
-// ADD HERE
-const DEFINITION_INDEX = 1;
-
 // Helper function for correct grammar when displaying data.
-const correctArticleForSpeechPart = function(partOfSpeech) {
+const correctArticleForSpeechPart = function (partOfSpeech) {
     switch (partOfSpeech.toLowerCase()) {
         case 'adjective':
         case 'article':
@@ -20,19 +14,19 @@ const correctArticleForSpeechPart = function(partOfSpeech) {
 };
 
 // Helper function for capitalizing the first letter of a given word.
-const capitalize = function(word) {
+const capitalize = function (word) {
     return word[0].toUpperCase() + word.slice(1);
 };
 
 // Helper function for clearing all children from a given node.
-const clearChildrenFromElement = function(node) {
+const clearChildrenFromElement = function (node) {
     while (node.firstChild) {
         node.removeChild(node.lastChild);
     }
 };
 
 // Helper function for creating table headers, with a given array of header names.
-const createTableHeaders = function(headers) {
+const createTableHeaders = function (headers) {
     console.log(headers.length);
     const tr = document.createElement('tr');
     headers.forEach((header) => {
@@ -45,7 +39,7 @@ const createTableHeaders = function(headers) {
 
 
 // Helper function for creating table rows, with a given array of data.
-const createTableRow = function(rowData) {
+const createTableRow = function (rowData) {
     console.log(rowData.length);
     const tr = document.createElement('tr');
     rowData.forEach((cell) => {
@@ -56,67 +50,60 @@ const createTableRow = function(rowData) {
     return tr;
 };
 
+/*
+TODO
+*/
 function getWord(word) {
     return fetch(`${API_PREFIX}${word}`)
         .then((response) => response.json())
         .then((data) => {
             console.log(data);
             //displayDefinitionsAsList(data);
-            displayDefinitionsAsTable(data);
+            //displayDefinitionsAsTable(data);
+            const definitionDisplay = document.getElementById('definition-display');
+            clearChildrenFromElement(definitionDisplay);
+            data.forEach((wordResult, wordResultIndex) => {
+                // Display phonetic and play pronunciation of the word.
+                const phonetic = document.createElement('h4');
+                phonetic.textContent = wordResult["phonetic"];
+                phonetic.addEventListener('mouseenter', (e) => {
+                    e.target.style.color = 'red';
+                    const audioObject = wordResult["phonetics"].find((phoneticObject) => phoneticObject.audio !== '');
+                    if (audioObject !== undefined) {
+                        new Audio(audioObject.audio).play();
+                    }
+                });
+                phonetic.addEventListener('mouseleave', (e) => e.target.style.color = 'black');
+                definitionDisplay.appendChild(phonetic);
+
+                /*
+                I substitute the word, "meaning", with "category". In the context
+                of the free dictionary API, is what I interpret to be a collection
+                of related definitions of a word.
+                */
+                wordResult["meanings"].forEach((category, categoryIndex) => {
+                    // Categorize
+                    const partOfSpeech = category["partOfSpeech"];
+                    const categoryHeader = document.createElement('h4');
+                    categoryHeader.textContent = `Category ${wordResultIndex + 1}.${categoryIndex + 1} - As ${correctArticleForSpeechPart(partOfSpeech)} ${capitalize(partOfSpeech)}`;
+                    const table = document.createElement('table');
+                    table.appendChild(createTableHeaders(['#', 'Definition', 'Example(s)']));
+
+                    // Display definition
+                    category["definitions"].forEach((definitionSubObject, definitionSubObjectIndex) => {
+                        // Retrieve example.
+                        let example = definitionSubObject["example"];
+                        example = (example === undefined) ? "N/A" : example;
+                        table.appendChild(createTableRow([
+                            `${wordResultIndex + 1}.${categoryIndex + 1}.${definitionSubObjectIndex + 1}`, 
+                            definitionSubObject["definition"], 
+                            example]));
+                    });
+                    definitionDisplay.appendChild(categoryHeader);
+                    definitionDisplay.appendChild(table);
+                });
+            });
         });
-}
-
-/* function displayDefinitionsAsList(data) {
-    const definitionsList = document.getElementById('definitions-list');
-    definitionsList.innerHTML = "";
-    for (let i = 0; i < data.length; i++) {
-        for (let j = 0; j < data[i]["meanings"].length; j++) {
-            for (let k = 0; k < data[i]["meanings"][j]["definitions"].length; k++) {
-                const li = document.createElement('li');
-                li.textContent = data[i]["meanings"][j]["definitions"][k]["definition"];
-                definitionsList.appendChild(li);
-            }
-        }
-    }
-} */
-
-function displayDefinitionsAsTable(data) {
-    const definitionDisplay = document.getElementById('definition-display');
-    clearChildrenFromElement(definitionDisplay);
-    for (let i = 0; i < data.length; i++) {
-        // Create phonetic and play audio.
-        const phonetic = document.createElement('h4');
-        phonetic.textContent = data[i]["phonetic"];
-        phonetic.addEventListener('mouseenter', (e) => {
-            e.target.style.color = 'red';
-            const audioObject = data[i]["phonetics"].find((phoneticObject) => phoneticObject.audio !== '');
-            new Audio(audioObject.audio).play();
-        });
-        phonetic.addEventListener('mouseleave', (e) => e.target.style.color = 'black');
-        definitionDisplay.appendChild(phonetic);
-
-        for (let j = 0; j < data[i]["meanings"].length; j++) {
-
-            const partOfSpeech = data[i]["meanings"][j]["partOfSpeech"];
-
-            // Create category
-            const category = document.createElement('h4');
-            category.textContent = `Category ${i + 1}.${j + 1} - As ${correctArticleForSpeechPart(partOfSpeech)} ${capitalize(partOfSpeech)}`;
-            const table = document.createElement('table');
-            table.appendChild(createTableHeaders(['#', 'Definition', 'Example(s)']));
-
-            // Create table data
-            for (let k = 0; k < data[i]["meanings"][j]["definitions"].length; k++) {
-                // Retrieve example.
-                let example = data[i]["meanings"][j]["definitions"][k]["example"];
-                example = (example === undefined) ? "N/A" : example;
-                table.appendChild(createTableRow([`${i+1}.${j+1}.${k+1}`, data[i]["meanings"][j]["definitions"][k]["definition"], example]));
-            }
-        definitionDisplay.appendChild(category);
-        definitionDisplay.appendChild(table);
-        }
-        
-    }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
